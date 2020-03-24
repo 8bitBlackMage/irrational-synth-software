4e7d685d5682093b3bbcd18a3514ac25bd23c596
8bitPandaPlugins <alexexley34@gmail.com>
2020-03-24 12:00:32 +0000

    removed build dir

--- Build/rtaudio/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alice/CLionProjects/IrrationalBackend/rtaudio/tests
-# Build directory: /home/alice/CLionProjects/IrrationalBackend/Build/rtaudio/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(apinames "/home/alice/CLionProjects/IrrationalBackend/Build/rtaudio/tests/apinames")
